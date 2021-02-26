c194a904e681d32649529f2c193ea9505880868a
Tech-Dex <rominu.alexandru@yahoo.com>
2021-02-26 17:32:44 +0200

    :art: Run lint.sh linter

--- commands.py
+++ commands.py
@@ -6,26 +6,26 @@ from discord.ext import commands
 SNOOZE = False
 
 
-@commands.command(name='_snooze')
+@commands.command(name="_snooze")
 async def snooze(ctx, *args):
     global SNOOZE
     if len(args) > 1 or len(args) == 0:
-        await ctx.send(f'The command form is: ``` $sudo_snooze <on> or <off> ```')
+        await ctx.send(f"The command form is: ``` $sudo_snooze <on> or <off> ```")
     elif len(args) == 1:
-        if str(args[0]).lower() == 'off':
+        if str(args[0]).lower() == "off":
             if SNOOZE is False:
-                await ctx.send(f'Snooze is already off!')
+                await ctx.send(f"Snooze is already off!")
             else:
                 SNOOZE = False
-                await ctx.send(f'Snooze is off')
-        elif str(args[0]).lower() == 'on':
+                await ctx.send(f"Snooze is off")
+        elif str(args[0]).lower() == "on":
             if SNOOZE is True:
-                await ctx.send(f'Snooze is already on!')
+                await ctx.send(f"Snooze is already on!")
             else:
                 SNOOZE = True
-                await ctx.send(f'Snooze is on!')
+                await ctx.send(f"Snooze is on!")
         else:
-            await ctx.send(f'The command form is: ``` $sudo_snooze <on> or <off> ```')
+            await ctx.send(f"The command form is: ``` $sudo_snooze <on> or <off> ```")
 
 
 @commands.command(name="_help")
@@ -34,16 +34,25 @@ async def help(ctx):
 
 
 def embed_help():
-    embed = discord.Embed(title=f'Help Page',
-                          color=discord.Colour(0x2ecc71),
-                          url=f'https://ibb.co/gdqz4ZM',
-                          description=f'For a better version please check GitHub.',
-                          )
-    embed.set_image(url='https://media.discordapp.net/attachments/811254175660638258/813764726160228352/logo.png')
-    embed.set_footer(text=f'GoGIG Bot '
-                          f'| {time.strftime("%a %b %d, %Y at %H:%M:%S", time.gmtime(time.time()))}')
-    embed.add_field(name=f'_snooze', value=f'Turn on/off option of sending logs about errors.\n'
-                                           f'Type $sudo_snooze command for more info.', inline=False)
-    embed.add_field(name=f'_help ', value=f'Shows this message', inline=False)
+    embed = discord.Embed(
+        title=f"Help Page",
+        color=discord.Colour(0x2ECC71),
+        url=f"https://ibb.co/gdqz4ZM",
+        description=f"For a better version please check GitHub.",
+    )
+    embed.set_image(
+        url="https://media.discordapp.net/attachments/811254175660638258/813764726160228352/logo.png"
+    )
+    embed.set_footer(
+        text=f"GoGIG Bot "
+        f'| {time.strftime("%a %b %d, %Y at %H:%M:%S", time.gmtime(time.time()))}'
+    )
+    embed.add_field(
+        name=f"_snooze",
+        value=f"Turn on/off option of sending logs about errors.\n"
+        f"Type $sudo_snooze command for more info.",
+        inline=False,
+    )
+    embed.add_field(name=f"_help ", value=f"Shows this message", inline=False)
 
     return embed
--- main.py
+++ main.py
@@ -1,40 +1,74 @@
 #! usr/bin/env python3
-import asyncpraw
-import pprint
-import time
-import asyncprawcore
 import asyncio
-import discord
 import os
+import pprint
 import sys
-from discord.ext import tasks, commands
+import time
+
+import asyncpraw
+import asyncprawcore
+import discord
+from discord.ext import commands, tasks
+
 import commands as bot_commands
 
 pp = pprint.PrettyPrinter(indent=4)
 
-name_subreddit_list = ['forhire', 'jobbit', 'freelance_forhire', 'RemoteJobs']
+name_subreddit_list = ["forhire", "jobbit", "freelance_forhire", "RemoteJobs"]
 sent_submission_id_list = list()
-keyword_job_list = ['developer', 'junior', 'mid', 'intermediate', 'senior', 'software', 'dev', 'devs',
-                    'backend', 'frontend', 'fullstack', 'web', 'full-stack',
-                    'front end', 'back end', 'front-end', 'back-end',
-                    'java', 'python', 'javascript', 'typescript', 'node', 'nodejs', 'deno', 'denojs',
-                    'angular', 'react', 'vue', 'django', 'flask', 'fastapi', 'spring', 'boot']
-illegal_char_list = ['.', ',', '!', '?', '[', ']']
+keyword_job_list = [
+    "developer",
+    "junior",
+    "mid",
+    "intermediate",
+    "senior",
+    "software",
+    "dev",
+    "devs",
+    "backend",
+    "frontend",
+    "fullstack",
+    "web",
+    "full-stack",
+    "front end",
+    "back end",
+    "front-end",
+    "back-end",
+    "java",
+    "python",
+    "javascript",
+    "typescript",
+    "node",
+    "nodejs",
+    "deno",
+    "denojs",
+    "angular",
+    "react",
+    "vue",
+    "django",
+    "flask",
+    "fastapi",
+    "spring",
+    "boot",
+]
+illegal_char_list = [".", ",", "!", "?", "[", "]"]
 
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-DISCORD_GUILD = os.getenv('DISCORD_GUILD')
-ADMIN_ROLE_ID = os.getenv('ADMIN_ROLE_ID')
-CHANNEL_TO_POST_ID = os.getenv('CHANNEL_TO_POST_ID')
-CHANNEL_LOGS_ID = os.getenv('CHANNEL_LOGS_ID')
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+DISCORD_GUILD = os.getenv("DISCORD_GUILD")
+ADMIN_ROLE_ID = os.getenv("ADMIN_ROLE_ID")
+CHANNEL_TO_POST_ID = os.getenv("CHANNEL_TO_POST_ID")
+CHANNEL_LOGS_ID = os.getenv("CHANNEL_LOGS_ID")
 GUILD_ID: int = -1
 
-reddit = asyncpraw.Reddit(client_id=f'{os.getenv("CLIENT_ID")}',
-                          client_secret=f'{os.getenv("CLIENT_SECRET")}',
-                          user_agent='Reddit Job Finder Discord Bot',
-                          username=f'{os.getenv("REDDIT_USER")}',
-                          password=f'{os.getenv("REDDIT_PASSWORD")}')
+reddit = asyncpraw.Reddit(
+    client_id=f'{os.getenv("CLIENT_ID")}',
+    client_secret=f'{os.getenv("CLIENT_SECRET")}',
+    user_agent="Reddit Job Finder Discord Bot",
+    username=f'{os.getenv("REDDIT_USER")}',
+    password=f'{os.getenv("REDDIT_PASSWORD")}',
+)
 
-client = commands.Bot(command_prefix='$sudo', help_command=None)
+client = commands.Bot(command_prefix="$sudo", help_command=None)
 
 
 @client.event
@@ -44,8 +78,8 @@ async def on_ready():
         if guild.name == DISCORD_GUILD:
             GUILD_ID = guild.id
             print(
-                f'{client.user} is connected to the following guild:\n'
-                f'{guild.name}(id: {guild.id})'
+                f"{client.user} is connected to the following guild:\n"
+                f"{guild.name}(id: {guild.id})"
             )
         else:
             sys.exit("The bot is not linked to the Guild you declared")
@@ -54,37 +88,41 @@ async def on_ready():
 def build_discord_embed_message(submission, keyword):
     title = submission.title
     if len(title) > 256:
-        title = submission.title[:252] + '...'
+        title = submission.title[:252] + "..."
     description = submission.selftext
     if len(description) > 2048:
-        description = submission.selftext[:2044] + '...'
-    embed = discord.Embed(title=f'{title}',
-                          color=discord.Colour(0x82de09),
-                          url=f'https://www.reddit.com{submission.permalink}',
-                          description=f'{description}',
-                          )
-    embed.set_image(url=f'')
-    embed.set_thumbnail(url=f'')
-    embed.set_author(name=f'{submission.author.name}')
-    embed.set_footer(text=f'Subreddit {submission.subreddit_name_prefixed} '
-                          f'| {time.strftime("%a %b %d, %Y at %H:%M:%S", time.gmtime(submission.created_utc))}')
+        description = submission.selftext[:2044] + "..."
+    embed = discord.Embed(
+        title=f"{title}",
+        color=discord.Colour(0x82DE09),
+        url=f"https://www.reddit.com{submission.permalink}",
+        description=f"{description}",
+    )
+    embed.set_image(url=f"")
+    embed.set_thumbnail(url=f"")
+    embed.set_author(name=f"{submission.author.name}")
+    embed.set_footer(
+        text=f"Subreddit {submission.subreddit_name_prefixed} "
+        f'| {time.strftime("%a %b %d, %Y at %H:%M:%S", time.gmtime(submission.created_utc))}'
+    )
     try:
         embed.set_thumbnail(url=f'{submission.preview["images"][0]["source"]["url"]}')
     except AttributeError:
         pass
-    embed.add_field(name=f'#️⃣', value=f'{keyword.capitalize()}', inline=False)
-    embed.add_field(name=f'👍', value=f'{submission.ups}', inline=True)
-    embed.add_field(name=f'👎', value=f'{submission.downs}', inline=True)
-    embed.add_field(name=f'💬', value=f'{submission.num_comments}', inline=True)
+    embed.add_field(name=f"#️⃣", value=f"{keyword.capitalize()}", inline=False)
+    embed.add_field(name=f"👍", value=f"{submission.ups}", inline=True)
+    embed.add_field(name=f"👎", value=f"{submission.downs}", inline=True)
+    embed.add_field(name=f"💬", value=f"{submission.num_comments}", inline=True)
 
     return embed
 
 
 def build_discord_embed_logs(e):
-    embed = discord.Embed(title=f'🚑 {e}',
-                          color=discord.Colour(0xe74c3c),
-                          description=f'{e.__doc__}',
-                          )
+    embed = discord.Embed(
+        title=f"🚑 {e}",
+        color=discord.Colour(0xE74C3C),
+        description=f"{e.__doc__}",
+    )
 
     return embed
 
@@ -102,13 +140,21 @@ async def mention_admin_in_case_of_exceptions(e):
     if GUILD_ID != -1:
         guild = client.get_guild(id=GUILD_ID)
         admin = discord.utils.get(guild.roles, id=int(ADMIN_ROLE_ID))
-        await channel.send(f'{admin.mention} I\'m sick, please help me!', embed=build_discord_embed_logs(e))
+        await channel.send(
+            f"{admin.mention} I'm sick, please help me!",
+            embed=build_discord_embed_logs(e),
+        )
 
 
-async def search_for_illegal_words_and_trigger_message_sending(word, keyword_job, submission):
+async def search_for_illegal_words_and_trigger_message_sending(
+    word, keyword_job, submission
+):
     for illegal_char in illegal_char_list:
-        word = word.replace(illegal_char, '')
-    if word.lower() == keyword_job.lower() and submission.id not in sent_submission_id_list:
+        word = word.replace(illegal_char, "")
+    if (
+        word.lower() == keyword_job.lower()
+        and submission.id not in sent_submission_id_list
+    ):
         await send_discord_message(submission, keyword_job)
         sent_submission_id_list.append(submission.id)
 
@@ -122,14 +168,20 @@ async def search_subreddits():
             async for submission in subreddit.new(limit=10):
                 for keyword_job in keyword_job_list:
                     if submission.link_flair_text:
-                        if 'hiring' in submission.link_flair_text.lower() and \
-                                submission.id not in sent_submission_id_list:
-                            for word in submission.permalink.replace('/', '_').split('_'):
-                                await search_for_illegal_words_and_trigger_message_sending(word, keyword_job,
-                                                                                           submission)
-                            for word in submission.selftext.split(' '):
-                                await search_for_illegal_words_and_trigger_message_sending(word, keyword_job,
-                                                                                           submission)
+                        if (
+                            "hiring" in submission.link_flair_text.lower()
+                            and submission.id not in sent_submission_id_list
+                        ):
+                            for word in submission.permalink.replace("/", "_").split(
+                                "_"
+                            ):
+                                await search_for_illegal_words_and_trigger_message_sending(
+                                    word, keyword_job, submission
+                                )
+                            for word in submission.selftext.split(" "):
+                                await search_for_illegal_words_and_trigger_message_sending(
+                                    word, keyword_job, submission
+                                )
         except asyncprawcore.exceptions.ServerError as e:
             if not bot_commands.SNOOZE:
                 await mention_admin_in_case_of_exceptions(e)
